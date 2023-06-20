fb4cab13ff93dcd682dbf49c9f6518da3c5228a8
vedantkesharia <keshariavedant@gmail.com>
2023-06-21 00:19:32 +0530

    initial commit

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -6,6 +6,7 @@ import { ChannelCard, Loader, VideoCard } from "./";
 const Videos = ({ videos, direction }) => {
   if(!videos?.length) return <Loader />;
   
+  
   return (
     <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
       {videos.map((item, idx) => (
